54dba06867ba2ef5fd675c3fe975551f0ba75f0c
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2024-09-26 19:25:52 +0200

    Section 3 preparation

---
+++ section3.php
@@ -0,0 +1,20 @@
+<!-- Section 3 -->
+<div id="section3-content">
+
+    <!-- Left arrow control -->
+    <div id="arrowL"></div>
+
+    <!-- Section 3 content -->
+    <div id="section3-centercontent">
+
+        <!-- Inner content (3 pages) -->
+        <div id="section3-centercontent-page1"></div>
+        <div id="section3-centercontent-page2"></div>
+        <div id="section3-centercontent-page3">
+            <iframe width="100%" src="https://online.fliphtml5.com/mavas/roaj/"></iframe>
+        </div>
+    </div>
+
+    <!-- Right arrow control -->
+    <div id="arrowR"></div>
+</div>
