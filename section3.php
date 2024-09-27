feeff8236abcc8eb39d85a446addacd439afbc29
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2024-09-27 07:15:30 +0200

    Fixed positioning issues in section3 and added background color on section 3 & 4

--- section3.php
+++ section3.php
@@ -13,6 +13,10 @@
         <!-- <div id="section3-centercontent-page1"></div> -->
         <!-- <div id="section3-centercontent-page2"></div> -->
         <div id="section3-centercontent-page3">
+        <br>
+            <h3>Studio Dumbar Promotional Book</h3>
+            <br>
+            <p>Studio Dumbar is an international design agency founded by Gert Dumbar. This book is a collection of work from Studio Dumbar which also includes their history, design philosophy, and more.</p>
             <iframe src="https://online.fliphtml5.com/mavas/roaj/"></iframe>
         </div>
 
